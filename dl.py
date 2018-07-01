6ad47974afbe0c54837c7f93fe959beea6fce020
mkeye <33815166+mkeye@users.noreply.github.com>
2018-06-30 23:38:20 -0700

    second commit

--- dl.py
+++ dl.py
@@ -2,5 +2,7 @@ def hello(data):
     return "hello " + data
 
 
-message = input("type in some data")
-hello(message)
+message = input("type in some data: ")
+response = hello(message)
+
+print(response)
