692784913d68793c5844276ce6e6c0a67b043281
mkeye <33815166+mkeye@users.noreply.github.com>
2018-07-01 11:10:11 -0700

    adding a package folder called dl

--- dl.py
+++ dl.py
@@ -1,8 +1,2 @@
 def hello(data):
     return "hello " + data
-
-
-message = input("type in some data: ")
-response = hello(message)
-
-print(response)
---
+++ main.py
@@ -0,0 +1,6 @@
+from dl import hello
+
+message = 'fuck you'
+
+response = hello(message)
+print(response)
